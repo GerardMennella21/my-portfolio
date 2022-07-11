95436ef3a227705a080457ee8512159470d42840
Gerard Mennella <gerardmennella21@gmail.com>
2022-07-11 14:38:16 -0400

    projects carousel

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel from 'react-bootstrap/Carousel'
 
 export default function Portfolio() {
+    const [projects] = useState([
+        {
+            name: "Budge-It",
+            type: "Full-Stack"
+        },
+        // {
+        //     name: "My-Social-API",
+        //     type: "Back-End"
+        // },
+        {
+            name: "Weather-Dashboard",
+            type: "Front-End"
+        },
+        // {
+        //     name: "Readme-Generator",
+        //     type: "Back-End"
+        // },
+        {
+            name: "Password-Generator",
+            type: "Front-End"
+        },
+        // {
+        //     name: "Portfolio-Generator",
+        //     type: "Back-End"
+        // },
+        {
+            name: "Work-Day-Scheduler",
+            type: "Front-End"
+        },
+        {
+            name: "Code-Quiz",
+            type: "Front-End"
+        },
+        {
+            name: "Take-A-Note",
+            type: "Front-End"
+        }
+        // {
+        //     name: "Worker-Tracker",
+        //     type: "Back-End"
+        // }
+    ])
+
     return (
-        <section>
-            
-        </section>
+        <Carousel fade id="my-projects">
+            {projects.map(project => (
+                <Carousel.Item>
+                    <img src={require(`../../assets/Projects/${project.name}.png`)} 
+                        className="d-block ms-auto me-auto rounded" 
+                        alt={project.name}
+                        key={project.name}
+                    />
+                </Carousel.Item>
+            ))}
+        </Carousel>
     )
 }
